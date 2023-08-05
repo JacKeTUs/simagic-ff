60ee7e350336108b7fdcf9c0aef453f4e04f73ef
Oleg <oleg@makarenk.ooo>
2023-08-06 01:04:56 +0300

    Now constant, spring, damper - works

--- hid-simagic.c
+++ hid-simagic.c
@@ -31,9 +31,7 @@
 struct smff_device {
 	struct hid_device *hid;
 	int pid_id[PID_EFFECTS_MAX];
-
-
-}
+};
 
 static const struct hid_device_id simagic_devices[] = {
 	{ HID_USB_DEVICE(USB_VENDOR_ID_SIMAGIC, USB_DEVICE_ID_SIMAGIC_ALPHA) },
@@ -59,36 +57,44 @@ static int sm_rescale_signed_to_10k(int i)
 	    -10000 / -0x8000;
 }
 
+static int get_block_id(struct ff_effect *effect) {
+	int sm_pid_id = 0;
+	switch (effect->type)
+	{
+	case FF_CONSTANT:
+		sm_pid_id = SM_CONSTANT;
+		break;
+	case FF_DAMPER:
+		sm_pid_id = SM_DAMPER;
+		break;
+	case FF_SPRING:
+		sm_pid_id = SM_SPRING;
+		break;
+	default:
+		break;
+	}
+	return sm_pid_id;
+}
+
 static int sm_set_constant_report(struct input_dev *dev, struct ff_effect *effect)
 {
-	int i;
-
 	struct hid_device *hid = input_get_drvdata(dev);
-	hid_info(dev, "Constant upload: type %u, id: %u\n", effect->type, effect->id);
-
 	struct list_head *report_list = &hid->report_enum[HID_OUTPUT_REPORT].report_list;
 	struct hid_report *report = list_entry(report_list->next, struct hid_report, list);
-
 	s32 *value = report->field[0]->value;
+
+	hid_info(dev, "Constant upload: type %u, id: %u\n", effect->type, effect->id);
 	hid_info(dev, "report id: %d", report->id);
 
 	for (int i = 0; i < 64; i++) {
 		value[i] = 0;
 	}
 	value[0] = SM_SET_CONSTANT_REPORT;
-	switch (effect->type)
-	{
-	case FF_CONSTANT:
-		value[1] = SM_CONSTANT;
-		break;
-	default:
-		break;
-	}
-
+	value[1] = get_block_id(effect);
 	hid_info(dev, "Const params: level: %d\n", 
 		effect->u.constant.level);
 
-	int mag = sm_rescale_signed_to_10k(effect->u.constant.level);//, 0x8000, 10000, 65535);
+	int mag = sm_rescale_signed_to_10k(effect->u.constant.level);
 	hid_info(dev, "Const params: level scaled: %d\n", 
 		mag);
 	value[2] = mag & 0x00ff;
@@ -100,32 +106,19 @@ static int sm_set_constant_report(struct input_dev *dev, struct ff_effect *effec
 
 static int sm_set_condition_report(struct input_dev *dev, struct ff_effect *effect)
 {
-	int i;
-
 	struct hid_device *hid = input_get_drvdata(dev);
-	hid_info(dev, "Condition upload: type %u, id: %u\n", effect->type, effect->id);
-
 	struct list_head *report_list = &hid->report_enum[HID_OUTPUT_REPORT].report_list;
 	struct hid_report *report = list_entry(report_list->next, struct hid_report, list);
-
 	s32 *value = report->field[0]->value;
+
+	hid_info(dev, "Condition upload: type %u, id: %u\n", effect->type, effect->id);
 	hid_info(dev, "report id: %d", report->id);
 
 	for (int i = 0; i < 64; i++) {
 		value[i] = 0;
 	}
 	value[0] = SM_SET_CONDITION_REPORT; // upd cond
-	switch (effect->type)
-	{
-	case FF_SPRING:
-		value[1] = SM_SPRING;
-		break;
-	case FF_DAMPER:
-		value[1] = SM_DAMPER;
-		break;
-	default:
-		break;
-	}
+	value[1] = get_block_id(effect);
 
 	hid_info(dev, "Condition[0] params: center %d, rightC %d, leftC %d, rightS %d, leftS %d, deadband %d\n", 
 		effect->u.condition[0].center, effect->u.condition[0].right_coeff, effect->u.condition[0].left_coeff, 
@@ -168,33 +161,21 @@ static int sm_set_condition_report(struct input_dev *dev, struct ff_effect *effe
 }
 
 static int sm_set_effect_report(struct input_dev *dev, struct ff_effect *effect) {
-	hid_info(dev, "NEW Effect upload: ef type: %d\n", effect->type);
 	struct hid_device *hid = input_get_drvdata(dev);
+	struct smff_device *smff = dev->ff->private;
 	struct list_head *report_list = &hid->report_enum[HID_OUTPUT_REPORT].report_list;
 	struct hid_report *report = list_entry(report_list->next, struct hid_report, list);
-
 	s32 *value = report->field[0]->value;
-	
-	for (int i = 0; i < 64; i++) {
+	int dur = effect->replay.length == 0 ? 0xffff: effect->replay.length;
+	int i;
+
+	hid_info(dev, "NEW Effect upload: ef type: %d\n", effect->type);
+	for (i = 0; i < 64; i++) {
 		value[i] = 0;
 	}
+	smff->pid_id[effect->id] = get_block_id(effect);
 	value[0] = SM_SET_EFFECT_REPORT;
-	switch (effect->type)
-	{
-	case FF_CONSTANT:
-		value[1] = SM_CONSTANT;
-		break;
-	case FF_SPRING:
-		value[1] = SM_SPRING;
-		break;
-	case FF_DAMPER:
-		value[1] = SM_DAMPER;
-		break;	
-	default:
-		break;
-	}
-	
-	int dur = effect->replay.length == 0 ? 0xffff: effect->replay.length;
+	value[1] = get_block_id(effect);
 	value[2] = 0x01; // Always 1??? Multiple effects of one type simultaniously??
 	value[3] = dur & 0x00ff; 		// Duration
 	value[4] = (dur & 0xff00) >> 8; // --
@@ -209,14 +190,11 @@ static int sm_set_effect_report(struct input_dev *dev, struct ff_effect *effect)
 
 static int sm_upload(struct input_dev *dev, struct ff_effect *effect, struct ff_effect *old) {
 	struct hid_device *hid = input_get_drvdata(dev);
-	hid_info(dev, "Effect upload: type %u, id: %u\n", effect->type, effect->id);
-
 	struct list_head *report_list = &hid->report_enum[HID_OUTPUT_REPORT].report_list;
 	struct hid_report *report = list_entry(report_list->next, struct hid_report, list);
 
-	s32 *value = report->field[0]->value;
+	hid_info(dev, "Effect upload: type %u, id: %u\n", effect->type, effect->id);
 	hid_info(dev, "report id: %d", report->id);
-	int i;
 	if (!old) {
 		sm_set_effect_report(dev, effect);
 	}
@@ -231,31 +209,29 @@ static int sm_upload(struct input_dev *dev, struct ff_effect *effect, struct ff_
 }
 
 static int sm_erase(struct input_dev *dev, int effect_id) {
-	struct hid_device *hid = input_get_drvdata(dev);
-	struct smff_device *pidff = dev->ff->private;
-	hid_info(dev, "Effect erase: id: %u\n", effect_id);
+	//struct hid_device *hid = input_get_drvdata(dev);
+	struct smff_device *smff = dev->ff->private;
+	hid_info(dev, "Effect erase: id: %d/%d\n", effect_id, smff->pid_id[effect_id]);
 	return 0;
 }
 
 
 
-static int sm_req_playback(struct input_dev *dev, int effect_id, int count) {
+static int sm_req_playback(struct input_dev *dev, int sm_block_id, int count) {
 	struct hid_device *hid = input_get_drvdata(dev);
-	//struct smff_device *pidff = dev->ff->private;
-	hid_info(dev, "Effect play: id: %u, count %d\n", effect_id, count);	
-
+	int i;
 	struct list_head *report_list = &hid->report_enum[HID_OUTPUT_REPORT].report_list;
 	struct hid_report *report = list_entry(report_list->next, struct hid_report, list);
-
 	s32 *value = report->field[0]->value;
+
+	hid_info(dev, "Effect play: sm_id: %u, count %d\n", sm_block_id, count);	
 	hid_info(dev, "report id: %d", report->id);
-	int i;
 
 	for (i = 0; i < 64; i++) {
 		value[i] = 0;
 	}
 	value[0] = SM_EFFECT_OPERATION_REPORT;
-	value[1] = effect_id; 
+	value[1] = sm_block_id; 
 	if (count > 0) {
 		value[2] = 0x01; // 1 - OpEffectStart
 		value[3] = count > 0xff ? 0xff : count; // loop count
@@ -270,51 +246,54 @@ static int sm_req_playback(struct input_dev *dev, int effect_id, int count) {
 }
 
 static int sm_playback(struct input_dev *dev, int effect_id, int count) {
-
-	// update all effects
+	struct smff_device *pidff = dev->ff->private;
 	hid_info(dev, "PLAYBACK: ID: %d, count %d\n", effect_id, count);
-	//sm_req_playback(dev, SM_SPRING, count);
-	//sm_req_playback(dev, SM_DAMPER, count);
-	sm_req_playback(dev, SM_CONSTANT, count);
-
+	sm_req_playback(dev, pidff->pid_id[effect_id], count);
 	return 0;
 }
 
 static void sm_set_gain(struct input_dev *dev, u16 gain) {
+	hid_info(dev, "Setting gain: %d\n", gain);
 }
 
 static void sm_set_autocenter(struct input_dev *dev, u16 magnitude) {
+	hid_info(dev, "Setting autocenter: %d\n", magnitude);
 }
-
 
 static int simagic_ff_initffb(struct hid_device *hid) {
 	struct hid_input *hidinput = list_entry(hid->inputs.next,
 						struct hid_input, list);
 	struct input_dev *dev = hidinput->input;
 	struct ff_device *ff;
+	struct smff_device *smff;
 	int max_effects = 16;
 	int error = -ENODEV;
 
-
+	smff = kzalloc(sizeof(*smff), GFP_KERNEL);
+	if (!smff)
+		return -ENOMEM;
+	smff->hid = hid;
+	
 	set_bit(FF_CONSTANT, dev->ffbit);
-	set_bit(FF_RAMP, dev->ffbit);
+	/*set_bit(FF_RAMP, dev->ffbit);
 	set_bit(FF_SQUARE, dev->ffbit);
 	set_bit(FF_SINE, dev->ffbit);
 	set_bit(FF_TRIANGLE, dev->ffbit);
 	set_bit(FF_SAW_UP, dev->ffbit);
-	set_bit(FF_SAW_DOWN, dev->ffbit);
+	set_bit(FF_SAW_DOWN, dev->ffbit);*/
 	set_bit(FF_SPRING, dev->ffbit);
 	set_bit(FF_DAMPER, dev->ffbit);
-	set_bit(FF_INERTIA, dev->ffbit);
+	/*set_bit(FF_INERTIA, dev->ffbit);
 	set_bit(FF_FRICTION, dev->ffbit);
 	set_bit(FF_PERIODIC, dev->ffbit);
-	set_bit(FF_CUSTOM, dev->ffbit);
+	set_bit(FF_CUSTOM, dev->ffbit);*/
 
 	error = input_ff_create(dev, max_effects);
 	if (error)
 		goto fail;
 	
 	ff = dev->ff;
+	ff->private = smff;
 	ff->upload = sm_upload;
 	ff->erase = sm_erase;
 	ff->playback = sm_playback;
