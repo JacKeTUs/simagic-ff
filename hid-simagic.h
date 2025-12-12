3f041ad6de8d1a07c95b58c3335e087e5b91c800
Oleg Makarenko <oleg@makarenk.ooo>
2025-12-12 03:39:44 +0200

    Add more ids

--- hid-simagic.h
+++ hid-simagic.h
@@ -2,8 +2,12 @@
 #ifndef __HID_SIMAGIC_H
 #define __HID_SIMAGIC_H
 
-#define USB_VENDOR_ID_SIMAGIC		0x0483
-#define USB_DEVICE_ID_SIMAGIC_WHEEL 0x0522
+#define USB_VENDOR_ID_SIMAGIC_ALPHA 0x0483
+#define USB_VENDOR_ID_SIMAGIC       0x3670
+#define USB_DEVICE_ID_SIMAGIC_ALPHA 0x0522
+#define USB_DEVICE_ID_SIMAGIC_EVO   0x0500
+#define USB_DEVICE_ID_SIMAGIC_EVO_1 0x0501
+#define USB_DEVICE_ID_SIMAGIC_EVO_2 0x0502
 
 int hid_pidff_init_simagic(struct hid_device *hdev);
 
